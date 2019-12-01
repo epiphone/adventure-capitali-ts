3e0332695a0832f2cbbe53adabb110691c420864
Aleksi Pekkala <aleksipekkala@gmail.com>
2019-12-01 23:56:10 +0900

    Updates

--- precache-manifest.771b4f37f0f070de0d5365c31a63afcb.js
+++ precache-manifest.771b4f37f0f070de0d5365c31a63afcb.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89f29c22baf612e9398146a0f77c3eb6",
+    "revision": "22844b008f8e0c0f0c2950885535a6c7",
     "url": "/adventure-capitali-ts/index.html"
   },
   {
-    "revision": "dbed2e25cf2be4d1af9d",
-    "url": "/adventure-capitali-ts/static/css/main.6b0f1771.chunk.css"
+    "revision": "d7718e3cd454358eecbb",
+    "url": "/adventure-capitali-ts/static/css/main.baf3395d.chunk.css"
   },
   {
     "revision": "fde0d58d998ceaed201a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/adventure-capitali-ts/static/js/2.1df4d760.chunk.js.LICENSE"
   },
   {
-    "revision": "dbed2e25cf2be4d1af9d",
-    "url": "/adventure-capitali-ts/static/js/main.a201d3f5.chunk.js"
+    "revision": "d7718e3cd454358eecbb",
+    "url": "/adventure-capitali-ts/static/js/main.d09a67e0.chunk.js"
   },
   {
     "revision": "d848771a802d0fb8fc3c",
