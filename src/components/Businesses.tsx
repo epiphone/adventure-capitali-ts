536451f496232e59d6da75241c2b5fa8e65d26b7
Aleksi Pekkala <aleksipekkala@gmail.com>
2019-12-01 19:32:56 +0900

    Render businesses responsively in either 1 or 2 columns

--- src/components/Businesses.tsx
+++ src/components/Businesses.tsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import * as Model from '../Model'
 import Business from './Business'
+import './Businesses.css'
 
 interface Props {
   dispatch: Model.Dispatch
   state: Model.State
 }
 
-const Businesses: React.FC<Props> = props => {
+/**
+ * Render businesses responsively in 1 or 2 columns.
+ */
+const Businesses: React.FC<Props> = ({ dispatch, state }) => {
+  const halfway = Math.ceil(state.businesses.length / 2)
+
   return (
-    <div>
-      {props.state.businesses.map((_, index) => (
-        <Business
-          key={index}
-          state={props.state}
-          dispatch={props.dispatch}
-          index={index}
-        />
-      ))}
+    <div className="Businesses">
+      <div>
+        {state.businesses.slice(0, halfway).map((business, index) => (
+          <Business
+            key={index}
+            business={business}
+            capital={state.capital}
+            dispatch={dispatch}
+            index={index}
+            upgradeMultiplier={state.upgradeMultiplier}
+          />
+        ))}
+      </div>
+      <div>
+        {state.businesses.slice(halfway).map((business, index) => (
+          <Business
+            key={index}
+            business={business}
+            capital={state.capital}
+            dispatch={dispatch}
+            index={halfway + index}
+            upgradeMultiplier={state.upgradeMultiplier}
+          />
+        ))}
+      </div>
     </div>
   )
 }
